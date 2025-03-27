5782086648198744f023bcf86e8c7e57f547a587
Elhassan_dahmouchi <hassandahmouchi0@gmail.com>
2025-03-27 22:37:43 +0000

    last

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
-import {NextConfig} from 'next';
-import createNextIntlPlugin from 'next-intl/plugin';
- 
-const nextConfig: NextConfig = {
-    i18n: {
-        locales: ["en", "fr"],
-        defaultLocale: "en",
-      },
-};
- 
+import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
+
 const withNextIntl = createNextIntlPlugin();
+
+const nextConfig: NextConfig = {};
+
 export default withNextIntl(nextConfig);
--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,5 +1,6 @@
 import {defineRouting} from 'next-intl/routing';
- 
+ import { createNavigation } from "next-intl/navigation";
+
 export const routing = defineRouting({
   // A list of all locales that are supported
   locales: ['en', 'fr'],
@@ -7,3 +8,6 @@ export const routing = defineRouting({
   // Used when no locale matches
   defaultLocale: 'en'
 });
+
+export const { Link, redirect, usePathname, useRouter } =
+  createNavigation(routing);
