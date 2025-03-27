41f9bb11f6d077c09986fc0b12dd26c8c6160499
Elhassan_dahmouchi <hassandahmouchi0@gmail.com>
2025-03-27 22:15:35 +0000

    hhh

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
  
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    i18n: {
+        locales: ["en", "fr"],
+        defaultLocale: "en",
+      },
+};
  
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
